5babbe9b9f547be1f69125fa8e803b76e3d57237
Jmikelittle <49447236+Jmikelittle@users.noreply.github.com>
2024-11-29 16:38:22 +0000

    Moved applied and infobase csvs to Resources folder, with script

--- Resources/retrieve_datasets.py
+++ Resources/retrieve_datasets.py
@@ -1,4 +1,11 @@
+import os
 import requests
+
+# Path to the folder where the script will save the files
+resources_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Resources')
+
+# Ensure the Resources folder exists
+os.makedirs(resources_folder, exist_ok=True)
 
 def download_and_fix_csv(url, filename):
     response = requests.get(url)
@@ -12,7 +19,8 @@ def download_and_fix_csv(url, filename):
         fixed_content = fixed_content.replace('\u2018', "'").replace('\u2019', "'")
         print("Fixed content snippet:", fixed_content[:100])  # Print the first 100 characters of the fixed content
         # Save the fixed content to a file with UTF-8 encoding
-        with open(filename, 'w', encoding='utf-8-sig') as file:
+        file_path = os.path.join(resources_folder, filename)
+        with open(file_path, 'w', encoding='utf-8-sig') as file:
             file.write(fixed_content)
         print(f'{filename} downloaded and fixed successfully!')
     else:

877355ee72c9037ae0fad889b3eff9c343a34ad5
Jmikelittle <49447236+Jmikelittle@users.noreply.github.com>
2025-01-22 20:57:44 +0000

    fixed accents

--- Resources/ministryDownload.py
+++ Resources/ministryDownload.py
@@ -7,7 +7,8 @@ url = "https://www.ourcommons.ca/members/en/ministries/csv"
 response = requests.get(url)
 
 # Save the content as 'ministries.csv' with utf-8-sig encoding to the Resources directory
-with open('Resources/ministries.csv', 'w', encoding='utf-8-sig') as file:
-    file.write(response.text)
+with open('Resources/ministries.csv', 'wb') as file:
+    file.write(b'\xef\xbb\xbf')  # Write BOM
+    file.write(response.content)
 
 print("The CSV file has been downloaded and saved as 'Resources/ministries.csv' with utf-8-sig encoding.")

85b5f97ef56d3ddc037d7b65de3974886cecf643
Jmikelittle <49447236+Jmikelittle@users.noreply.github.com>
2025-02-03 21:03:43 +0000

    ministers integrated into GC Org Info

--- Resources/fixLeadDepartment.py
+++ Resources/fixLeadDepartment.py
@@ -16,22 +16,8 @@ manual_ministries_df = pd.read_csv(manual_ministries_file)
 manual_lead_department_df['Parent GC OrgID'] = manual_lead_department_df['Parent GC OrgID'].astype(str).str.split('.').str[0]
 harmonized_names_df['GC OrgID'] = harmonized_names_df['GC OrgID'].astype(str)
 
-# Debugging: Print the first few rows of the dataframes to check the data types and values
-print("Manual Lead Department DataFrame:")
-print(manual_lead_department_df.head())
-
-print("\nHarmonized Names DataFrame:")
-print(harmonized_names_df.head())
-
-print("\nManual Ministries DataFrame:")
-print(manual_ministries_df.head())
-
 # Merge the dataframes on 'GC OrgID' from create_harmonized_name.csv and 'Parent GC OrgID' from Manual_leadDepartmentPortfolio.csv
 merged_df = pd.merge(manual_lead_department_df, harmonized_names_df[['GC OrgID', 'harmonized_name', 'nom_harmonisé']], left_on='Parent GC OrgID', right_on='GC OrgID', how='left')
-
-# Debugging: Print the first few rows of the merged dataframe to check the merge result
-print("\nMerged DataFrame:")
-print(merged_df.head())
 
 # Replace values in 'lead department' with 'harmonized_name' only if not already filled
 merged_df['lead department'] = merged_df.apply(
@@ -43,13 +29,16 @@ merged_df['ministère responsable'] = merged_df.apply(
     lambda row: row['nom_harmonisé'] if pd.isna(row['ministère responsable']) else row['ministère responsable'], axis=1
 )
 
-# Fill lead department with values from manualMinistries.csv if Parent GC OrgID starts with 'm'
+# Fill lead department and ministère responsable with values from manualMinistries.csv if Parent GC OrgID starts with 'm'
 for index, row in merged_df.iterrows():
     if str(row['Parent GC OrgID']).startswith('m'):
         min_id = row['Parent GC OrgID']
         title_value = manual_ministries_df.loc[manual_ministries_df['minID'] == min_id, 'Title'].values
+        titre_value = manual_ministries_df.loc[manual_ministries_df['minID'] == min_id, 'Titre'].values
         if len(title_value) > 0:
             merged_df.at[index, 'lead department'] = title_value[0]
+        if len(titre_value) > 0:
+            merged_df.at[index, 'ministère responsable'] = titre_value[0]
 
 # Drop the 'harmonized_name', 'nom_harmonisé', and 'GC OrgID_y' columns as they are no longer needed
 merged_df = merged_df.drop(columns=['harmonized_name', 'nom_harmonisé', 'GC OrgID_y'])
